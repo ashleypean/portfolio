deebf16d6face95b1283d4c00e5b01c9e2fe10a1
Ashley Pean <pean.ashley@gmail.com>
2021-04-18 07:27:26 -0400

    removed image component

--- main/src/components/Header.js
+++ main/src/components/Header.js
@@ -1,22 +1,13 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import LineIcon from 'react-lineicons';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
-  const [information, setInformation] = useState('');
   const [navigationToggler, setNavigationToggler] = useState(false);
 
   const handleNavigationToggler = () => {
     setNavigationToggler(!navigationToggler);
   };
-
-  useEffect(() => {
-    axios.get('/api/information')
-      .then((response) => {
-        setInformation(response.data);
-      });
-  }, []);
 
   return (
     <nav className={navigationToggler ? 'mi-header is-visible' : 'mi-header'}>
@@ -25,9 +16,7 @@ function Header() {
       </button>
       <div className="mi-header-inner">
         <div className="mi-header-image">
-          <Link to="/">
-            <img src={information.brandImage} alt="brandimage" />
-          </Link>
+          <div style={{ height: 150 }} />
         </div>
 
         <ul className="mi-header-menu">
@@ -35,7 +24,7 @@ function Header() {
           <li><NavLink to="/about"><span>About</span></NavLink></li>
           <li><NavLink to="/resume"><span>Resume</span></NavLink></li>
           <li><NavLink to="/portfolios"><span>Portfolios</span></NavLink></li>
-          <li><NavLink to="/blogs"><span>Blogs</span></NavLink></li>
+          <li><NavLink to="/blog"><span>Blog</span></NavLink></li>
           <li><NavLink to="/contact"><span>Contact</span></NavLink></li>
         </ul>
         <p className="mi-header-copyright">
